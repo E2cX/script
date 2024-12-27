50e45e4275eb278598e5d179a5b8716b351e0d1e
‪Xp0‬‏ <kalidsehriqopl@gmail.com>
2024-12-28 02:08:53 +0300

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -2,7 +2,7 @@ local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https
 elseif game.PlaceId == 13772394625 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/E2cX/script/refs/heads/main/Blade%20Ball.txt", true))()
 elseif game.PlaceId == 8737602449 then 
-    loadstring(game:HttpGet("hhttps://raw.githubusercontent.com/E2cX/script/refs/heads/main/pls%20donate.txt", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/E2cX/script/refs/heads/main/pls%20donate.txt", true))()
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/E2cX/script/refs/heads/main/blox%20fruit", true))()
 elseif game.PlaceId == 4520749081 or  game.PlaceId == 6381829480 or game.PlaceId == 15759515082 or game.PlaceId == 5931540094 then 
